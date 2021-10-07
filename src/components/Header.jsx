ec41b3f16c604dc35077e2bae80b0e02fbccc953
singh-anshuman <anshumansingh137@gmail.com>
2021-10-07 16:57:49 +0530

    Added react-bootstrap and added navbar to the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Container, Navbar, NavDropdown, Nav } from 'react-bootstrap';
 
 export default class Header extends React.Component {
     render() {
         return(
-            <div>
-                Header
-            </div>
+            <Navbar bg="dark" expand="lg" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Investment Research</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Dashboard</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         )
     }
 }
