05a87f1e751b43290a3d15aabb3d20ada0e6da8a
singh-anshuman <anshumansingh137@gmail.com>
2021-10-07 09:48:04 +0530

    Added Header and Body components to the app

---
+++ src/components/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dashboard from './Dashboard';
+
+export default class Body extends React.Component {
+    render() {
+        return(
+            <div>
+                <Dashboard/>
+            </div>
+        )
+    }
+}
