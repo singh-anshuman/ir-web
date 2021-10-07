075e5a49f43b44c6148131df07a330d3dbdea57e
singh-anshuman <anshumansingh137@gmail.com>
2021-10-07 10:11:33 +0530

    Modified Dashboard text

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,7 +4,7 @@ export default class Dashboard extends React.Component {
     render() {
         return (
             <div>
-                This is the my first React component after a long time !
+                Dashboard Component !
             </div>
         )
     }
