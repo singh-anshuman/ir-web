5d7d895f0923d3ef6bda72e956e53c189335ab31
singh-anshuman <anshumansingh137@gmail.com>
2021-10-09 11:39:50 +0530

    Added ant-design and removed bootstrap and react-bootstrap from the application

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
 import Header from './components/Header';
 import Body from './components/Body';
-import {Container} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        <Header/>
-        <Body/>
-      </Container>
+      <Header/>
+      <Body/>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import { Container, Navbar, Nav } from 'react-bootstrap';
 
 export default class Header extends React.Component {
     render() {
         return(
-            <Navbar bg="light" expand="lg" variant="light">
-                <Container>
-                    <Navbar.Brand href="#home">Investment Research</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                </Container>
-            </Navbar>
+            <div>
+                Investment Research
+            </div>
         )
     }
 }
