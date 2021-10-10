a12c2fd56d882bb9b68372000c74e1af67f1356e
singh-anshuman <anshumansingh137@gmail.com>
2021-10-10 12:46:09 +0530

    Modified add investment dialog to add inputs for investment fields

--- src/components/AddInvestment/AddInvestment.jsx
+++ src/components/AddInvestment/AddInvestment.jsx
@@ -44,28 +44,24 @@ export default class AddInvestment extends React.Component {
                     autoComplete="off"
                     >
                     <Form.Item
-                        label="Username"
-                        name="username"
-                        rules={[{ required: true, message: 'Please input your username!' }]}
+                        label="ID"
+                        name="id"
+                        rules={[{ required: true, message: 'Please input investment ID!' }]}
                     >
                         <Input />
                     </Form.Item>
 
                     <Form.Item
-                        label="Password"
-                        name="password"
-                        rules={[{ required: true, message: 'Please input your password!' }]}
+                        label="Symbol"
+                        name="symbol"
+                        rules={[{ required: true, message: 'Please input synbol!' }]}
                     >
-                        <Input.Password />
-                    </Form.Item>
-
-                    <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-                        <Checkbox>Remember me</Checkbox>
+                        <Input />
                     </Form.Item>
 
                     <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
                         <Button type="primary" htmlType="submit">
-                        Submit
+                            Submit
                         </Button>
                     </Form.Item>
                 </Form>
